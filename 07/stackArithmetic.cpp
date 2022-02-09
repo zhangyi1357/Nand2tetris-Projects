a4954c6c90f555249affa442fe47456dc29815c9
Zhang Yi <1403287433@qq.com>
2022-02-09 14:59:39 +0800

    SimpleAdd test finished

--- 07/stackArithmetic.cpp
+++ 07/stackArithmetic.cpp
@@ -3,21 +3,22 @@
 #include <vector>
 #include <fstream>
 #include <map>
+#include <cassert>
 #include "dirent.h"
 using namespace std;
 
 vector<string> filefinder(string path, string const& postfix = ".vm");
 bool hasEnding(string const& fullString, string const& ending = ".vm");
 string getOutputFilename(const string& inputFilename);
-
 enum class CommandType { C_ARITHMETIC, C_PUSH, C_POP, C_LABEL, C_GOTO, C_IF, C_FUNCTION, C_RETURN, C_CALL };
+
 class Parser {
 public:
     Parser(string filename);
     bool hasMoreCommands();
     CommandType commandType();
-    string arg1();
-    string arg2();
+    string arg1(CommandType cmdType);
+    int arg2();
 private:
     vector<string> lines;
     int lineNum; // current working line number (initial -1)
@@ -30,6 +31,8 @@ public:
     void writeArithmetic(string cmd);
     void writePushPop(CommandType cmdType, string segment, int index);
     void close();
+private:
+    ofstream fout;
 };
 
 int main(int argc, char** argv) {
@@ -48,15 +51,17 @@ int main(int argc, char** argv) {
         while (parser.hasMoreCommands()) {
             CommandType cmdType = parser.commandType();
             if (cmdType == CommandType::C_ARITHMETIC) {
-
+                string cmd = parser.arg1(cmdType);
+                codeWriter.writeArithmetic(cmd);
             }
             else if (cmdType == CommandType::C_PUSH || cmdType == CommandType::C_POP) {
-
+                codeWriter.writePushPop(cmdType, parser.arg1(cmdType), parser.arg2());
             }
             else {
-                cout << "not finished yet." << endl;
+                cout << "Not finished yet." << endl;
             }
         }
+        codeWriter.close();
     }
     return 0;
 }
@@ -109,7 +114,8 @@ Parser::Parser(string filename) :lineNum(-1) {
             line.erase(0, found);
         found = line.find_last_not_of(" ");
         if (found != string::npos)
-            line.erase(found);
+            line.erase(found + 1);
+        // if line is empty drop it
         if (!line.empty()) {
             lines.push_back(line);
             cout << line << endl;
@@ -148,4 +154,57 @@ CommandType Parser::commandType() {
     else
         cout << "not valid cmdtype: " << cmd;
     return cmdType;
+}
+
+string Parser::arg1(CommandType cmdType) {
+    assert(cmdType != CommandType::C_RETURN);
+    string arg1;
+    if (cmdType == CommandType::C_ARITHMETIC)
+        arg1 = lines[lineNum];
+    else {
+        size_t firstSpace = lines[lineNum].find_first_of(" ");
+        size_t lastSpace = lines[lineNum].find_last_of(" ");
+        if (lastSpace != firstSpace)
+            arg1 = lines[lineNum].substr(firstSpace + 1, lastSpace - firstSpace - 1);
+        else
+            arg1 = lines[lineNum].substr(firstSpace + 1, string::npos);
+    }
+    cout << "arg1: " << arg1 << endl;
+    return arg1;
+}
+
+int Parser::arg2() {
+    size_t lastSpace = lines[lineNum].find_last_of(" ");
+    string arg2 = lines[lineNum].substr(lastSpace + 1);
+    // cout << arg2 << ' ' << arg2.size() << endl;
+    return stoi(arg2);
+}
+
+void CodeWriter::setFileName(string filename) {
+    fout.open(filename);
+}
+
+void CodeWriter::close() {
+    fout << "(END)\n@END\n0;JMP\n";
+    fout.close();
+}
+
+void CodeWriter::writeArithmetic(string cmd) {
+    if (cmd == "add") {
+        fout << "@SP\nM=M-1\nA=M\nD=M\n@SP\nM=M-1\nA=M\nM=D+M\n@SP\nM=M+1\n";
+    }
+}
+
+void CodeWriter::writePushPop(CommandType cmdType, string segment, int index) {
+    assert(cmdType == CommandType::C_PUSH || cmdType == CommandType::C_POP);
+    cout << "segment: " << segment << segment.size() << endl;
+    if (cmdType == CommandType::C_PUSH) {
+        if (segment == "constant")
+            fout << "@" << index << "\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1\n";
+        else
+            cout << "push: only constant now.\n";
+    }
+    else {
+        cout << "Pop not finished yet" << endl;
+    }
 }
